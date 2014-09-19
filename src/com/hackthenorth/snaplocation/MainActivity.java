36cf944721bc1b91f0ecaeaa09164c0720e6a51e
Disiok <simon_suo@hotmail.com>
2014-09-19 11:04:57 -0400

    Add more standard implementation of image capture and saving media file

--- src/com/hackthenorth/snaplocation/MainActivity.java
+++ src/com/hackthenorth/snaplocation/MainActivity.java
@@ -1,28 +1,84 @@
 package com.hackthenorth.snaplocation;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
+	public static final String TAG = MainActivity.class.getSimpleName();
+	public static final String IMG_DIR = "SnapLocation";
+	// View elements
 	Camera mCamera;
 	CameraView mCameraView;
+	Button mCaptureButton;
+
+	PictureCallback mPictureCallback;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-        // Create an instance of Camera
-        mCamera = Utils.getCameraInstance();
-        
-        // Create our Preview view and set it as the content of our activity.
-        mCameraView = new CameraView(this, mCamera);
-        FrameLayout cameraViewContainer = (FrameLayout) findViewById(R.id.camera_preview);
-        cameraViewContainer.addView(mCameraView);
+
+		// Resolve view elements
+		mCaptureButton = (Button) findViewById(R.id.capture_button);
+
+		// Create an instance of Camera
+		mCamera = Utils.getCameraInstance();
+
+		// Create our Preview view and set it as the content of our activity.
+		mCameraView = new CameraView(this, mCamera);
+		FrameLayout cameraViewContainer = (FrameLayout) findViewById(R.id.camera_preview);
+		cameraViewContainer.addView(mCameraView);
+
+		// Create picture callback
+		mPictureCallback = new PictureCallback() {
+
+			@Override
+			public void onPictureTaken(byte[] data, Camera camera) {
+				File pictureFile = getOutputMediaFile();
+				if (pictureFile == null) {
+					Log.d(TAG,
+							"Error creating media file, check storage permissions");
+					return;
+				}
+
+				try {
+					FileOutputStream outputStream = new FileOutputStream(
+							pictureFile);
+					outputStream.write(data);
+					outputStream.close();
+				} catch (FileNotFoundException e) {
+					Log.d(TAG, "File not found: " + e.getMessage());
+				} catch (IOException e) {
+					Log.d(TAG, "Error accessing file: " + e.getMessage());
+				}
+			}
+
+		};
+
+		mCaptureButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// get an image from the camera
+				mCamera.takePicture(null, null, mPictureCallback);
+			}
+		});
 	}
 
 	@Override
@@ -42,5 +98,43 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		releaseCamera(); // release the camera immediately on pause event
+	}
+
+	private void releaseCamera() {
+		if (mCamera != null) {
+			mCamera.release(); // release the camera for other applications
+			mCamera = null;
+		}
+	}
+
+	private File getOutputMediaFile() {
+		File mediaStorageDir = new File(
+				Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				IMG_DIR);
+		// This location works best if you want the created images to be shared
+		// between applications and persist after your app has been uninstalled.
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				Log.d(TAG, "failed to create directory");
+				return null;
+			}
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+		File mediaFile = new File(mediaStorageDir.getPath() + File.separator
+				+ "IMG_" + timeStamp + ".jpg");
+
+		return mediaFile;
 	}
 }
