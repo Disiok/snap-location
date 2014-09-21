cc14ff681e43609feeb65c3b9c085ad0ea7aa318
Disiok <simon_suo@hotmail.com>
2014-09-21 00:13:18 -0400

    Cleanup

--- src/com/hackthenorth/snaplocation/GPSTracker.java
+++ src/com/hackthenorth/snaplocation/GPSTracker.java
@@ -1,4 +1,5 @@
 package com.hackthenorth.snaplocation;
+
 import android.app.AlertDialog;
 import android.app.Service;
 import android.content.Context;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.util.Log;
+
 public class GPSTracker extends Service implements LocationListener {
 	private final Context mContext;
 	// flag for GPS status
@@ -28,10 +30,12 @@ public class GPSTracker extends Service implements LocationListener {
 	private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
 	// Declaring a Location Manager
 	protected LocationManager locationManager;
+
 	public GPSTracker(Context context) {
 		this.mContext = context;
 		getLocation();
 	}
+
 	public Location getLocation() {
 		try {
 			locationManager = (LocationManager) mContext
@@ -44,13 +48,14 @@ public class GPSTracker extends Service implements LocationListener {
 					.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 			if (!isGPSEnabled && !isNetworkEnabled) {
 				// no network provider is enabled
-				Log.d("Network","Missing");
+				Log.d("Network", "Missing");
 			} else {
 				this.canGetLocation = true;
 				// First get location from Network Provider
 				if (isNetworkEnabled) {
 					locationManager.requestLocationUpdates(
-							LocationManager.NETWORK_PROVIDER,MIN_TIME_BW_UPDATES,
+							LocationManager.NETWORK_PROVIDER,
+							MIN_TIME_BW_UPDATES,
 							MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
 					Log.d("Network", "Network");
 					if (locationManager != null) {
@@ -90,20 +95,20 @@ public class GPSTracker extends Service implements LocationListener {
 	}
 
 	/**
-     * Stop using GPS listener
-     * Calling this function will stop using GPS in your app
-     * */
-	public void stopUsingGPS(){
-		if(locationManager != null){
+	 * Stop using GPS listener Calling this function will stop using GPS in your
+	 * app
+	 * */
+	public void stopUsingGPS() {
+		if (locationManager != null) {
 			locationManager.removeUpdates(GPSTracker.this);
 		}
 	}
 
 	/**
-     * Function to get latitude
-     * */
-	public double getLatitude(){
-		if(location != null){
+	 * Function to get latitude
+	 * */
+	public double getLatitude() {
+		if (location != null) {
 			latitude = location.getLatitude();
 		}
 
@@ -112,10 +117,10 @@ public class GPSTracker extends Service implements LocationListener {
 	}
 
 	/**
-     * Function to get longitude
-     * */
-	public double getLongitude(){
-		if(location != null){
+	 * Function to get longitude
+	 * */
+	public double getLongitude() {
+		if (location != null) {
 			longitude = location.getLongitude();
 		}
 		// return longitude
@@ -123,38 +128,43 @@ public class GPSTracker extends Service implements LocationListener {
 	}
 
 	/**
-     * Function to check GPS/wifi enabled
-     * @return boolean
-     * */
+	 * Function to check GPS/wifi enabled
+	 * 
+	 * @return boolean
+	 * */
 	public boolean canGetLocation() {
 		return this.canGetLocation;
 	}
 
 	/**
-     * Function to show settings alert dialog
-     * On pressing Settings button will lauch Settings Options
-     * */
-	public void showSettingsAlert(){
+	 * Function to show settings alert dialog On pressing Settings button will
+	 * lauch Settings Options
+	 * */
+	public void showSettingsAlert() {
 		AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
 
 		// Setting Dialog Title
 		alertDialog.setTitle("GPS is settings");
 		// Setting Dialog Message
-		alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+		alertDialog
+				.setMessage("GPS is not enabled. Do you want to go to settings menu?");
 		// On pressing Settings button
-		alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog,int which) {
-				Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-				mContext.startActivity(intent);
-			}
-		});
+		alertDialog.setPositiveButton("Settings",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						Intent intent = new Intent(
+								Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+						mContext.startActivity(intent);
+					}
+				});
 
 		// on pressing cancel button
-		alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.cancel();
-			}
-		});
+		alertDialog.setNegativeButton("Cancel",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.cancel();
+					}
+				});
 		// Showing Alert Message
 		alertDialog.show();
 	}
@@ -166,6 +176,7 @@ public class GPSTracker extends Service implements LocationListener {
 	@Override
 	public void onProviderDisabled(String provider) {
 	}
+
 	@Override
 	public void onProviderEnabled(String provider) {
 	}
